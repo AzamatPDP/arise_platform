2147a7d150e896f8a423ad9c994607d57652c434
Azamat Jumabayev <jumabayevazamat8774@gmail.com>
2026-03-16 23:08:35 +0800

    Custom 404 error page added

--- config/settings.py
+++ config/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3@mx#_79sp@@8yc7@i6leh!#h$o-jx6t@2*vhnqw$a69gg9yk*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
